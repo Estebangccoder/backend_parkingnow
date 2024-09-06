f61473c8a319d1f4bd3fcf82268287aef6fa5d64
mspena14 <marlono1naranjo@gmail.com>
2024-09-06 14:37:26 -0500

    feat: bookings entity without connections created

---
+++ src/bookings/entities/booking.entity.ts
@@ -0,0 +1,28 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('bookings')
+export class Booking {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({type: 'date'})
+    star_date: Date;
+
+    @Column({type: 'time'})
+    start_time: string;
+
+    @Column({type: 'decimal'})
+    amount: number;
+
+    @Column({type: 'date'})
+    end_date: Date;
+
+    @Column({type: 'time'})
+    end_time: string;
+
+    @CreateDateColumn({type: 'timestamp'})
+    creartedAt: Date;
+
+    @UpdateDateColumn({type: 'timestamp'})
+    updatedAt: Date;
+}
