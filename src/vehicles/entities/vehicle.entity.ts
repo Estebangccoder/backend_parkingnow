412c37112256170a00c50e06ac688a68fa087ddf
mspena14 <marlono1naranjo@gmail.com>
2024-09-08 22:02:22 -0500

    feat: vehicles table connections added

--- src/vehicles/entities/vehicle.entity.ts
+++ src/vehicles/entities/vehicle.entity.ts
@@ -1,35 +1,37 @@
+import { VehicleType } from 'src/common/entities/vehicle_type.entity';
+import { User } from 'src/users/entities/user.entity';
 import {
-    Entity,
-    Column,
-    PrimaryGeneratedColumn,
-    CreateDateColumn,
-    UpdateDateColumn,
-}from 'typeorm';
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
 
 
 @Entity('vehicles')
-export class Vehicle{
+export class Vehicle {
 
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @Column('int')
-    driver_id: number;
+  @Column({ type: 'varchar', length: 10, unique: true })
+  plate: string;
 
-    @Column('int')
-    vehicle_type_id: number;
+  @CreateDateColumn({type: 'timestamp'})
+  createdAt: Date;
 
-    @Column('varchar')
-    plate: string;
+  @UpdateDateColumn({type: 'timestamp'})
+  updatedAt: Date;
 
-    @CreateDateColumn({
-        type: 'timestamp',
-      })
-      createdAt: Date;
-    
-    @UpdateDateColumn({
-        type: 'timestamp',
-      })
-      updatedAt: Date;
-    
+  @ManyToOne(() => User, user => user.vehicles)
+  @JoinColumn({ name: 'driver_id' })
+  driverId: User;
+
+  @ManyToOne(() => VehicleType, (vehicleType) => vehicleType.vehicles)
+  @JoinColumn({ name: 'vehicle_type_id' })
+  vehicleType: VehicleType;
+
 }
