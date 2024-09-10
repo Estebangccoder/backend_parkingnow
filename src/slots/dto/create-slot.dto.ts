9103263e7bea40ef50482130de17d96247e843db
mspena14 <marlono1naranjo@gmail.com>
2024-09-09 21:37:26 -0500

    feat: create-slot.dto added

--- src/slots/dto/create-slot.dto.ts
+++ src/slots/dto/create-slot.dto.ts
@@ -1,1 +1,47 @@
-export class CreateSlotDto {}
+import {
+    IsBoolean,
+    IsBooleanString,
+    IsDecimal,
+    IsNotEmpty,
+    IsNumberString,
+    IsOptional,
+    IsString,
+    IsUUID,
+    Length
+} from 'class-validator'
+export class CreateSlotDto {
+    @IsOptional()
+    @IsString()
+    @Length(0, 100)
+    name: string;
+
+    @IsOptional()
+    @IsString()
+    @Length(0, 255)
+    description: string;
+
+    @IsNotEmpty()
+    @IsDecimal()
+    hourPrice: number;
+
+    @IsNotEmpty()
+    @IsBooleanString()
+    isAvailable: boolean;
+
+    @IsNotEmpty()
+    @IsBooleanString()
+    isCovered: boolean;
+
+    @IsNotEmpty()
+    @IsString()
+    vehicleType: string;
+
+    @IsNotEmpty()
+    @IsString()
+    propertyId: string;
+
+    @IsNotEmpty()
+    @IsString()
+    ownerId: string;
+
+}
