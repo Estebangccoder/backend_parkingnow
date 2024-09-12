b1e8ebfa49a9b79436e41df74584eebaca900098
mspena14 <marlono1naranjo@gmail.com>
2024-09-11 21:25:16 -0500

    feat: CRUD connected to the database created

--- src/slots/slots.service.ts
+++ src/slots/slots.service.ts
@@ -1,31 +1,88 @@
-import { Injectable } from '@nestjs/common';
-import { CreateSlotDto } from './dto/create-slot.dto';
-import { UpdateSlotDto } from './dto/update-slot.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Slot } from './entities/slot.entity';
-import { Repository } from 'typeorm';
+import {
+  BadRequestException,
+  HttpException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from "@nestjs/common";
+import { CreateSlotDto } from "./dto/create-slot.dto";
+import { UpdateSlotDto } from "./dto/update-slot.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Slot } from "./entities/slot.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class SlotsService {
-  constructor(@InjectRepository(Slot) private readonly slotRepositoy: Repository<Slot>){}
+  constructor(
+    @InjectRepository(Slot) private readonly slotRepository: Repository<Slot>
+  ) {}
 
   async create(slot: CreateSlotDto) {
-    return this.slotRepositoy.create(slot);
+    try {
+      const createdSlot = this.slotRepository.create(slot);
+
+      return await this.slotRepository.save(createdSlot);
+    } catch (error) {
+      throw new HttpException(
+        error.message || "Internal server error",
+        error.status || 500
+      );
+    }
   }
 
-  findAll() {
-    return this.slotRepositoy.find();
+  async findAll() {
+    try {
+      return await this.slotRepository.find();
+    } catch (error) {
+      throw new HttpException(
+        error.message || "Internal server error",
+        error.status || 500
+      );
+    }
   }
 
-  findOne(id: string) {
-    return this.slotRepositoy.findOne({where: {id}});
+  async findOne(id: string) {
+    try {
+      const slot = await this.slotRepository.findOne({
+        where: { id },
+        relations: ["owner"],
+      });
+
+      if (!slot) throw new NotFoundException("Slot not found");
+
+      return slot;
+    } catch (error) {
+      throw new HttpException(
+        error.message || "Internal server error",
+        error.status || 500
+      );
+    }
   }
 
-  update(id: string, updateSlotDto: UpdateSlotDto) {
-    return this.slotRepositoy.update(id, updateSlotDto);
+  async update(id: string, updateSlotDto: UpdateSlotDto) {
+    try {
+      const slot = this.findOne(id);
+
+      if (!slot) throw new NotFoundException("slot not fount");
+
+      return this.slotRepository.save(updateSlotDto);
+    } catch (error) {}
   }
 
-  remove(id: string) {
-    return this.slotRepositoy.delete(id);
+  async remove(id: string) {
+    try {
+      const slot = await this.findOne(id);
+
+      if (!slot) throw new NotFoundException("Slot not found");
+
+      return await this.slotRepository.remove(slot);
+
+      return slot;
+    } catch (error) {
+      throw new HttpException(
+        error.message || "Internal server error",
+        error.status || 500
+      );
+    }
   }
 }
