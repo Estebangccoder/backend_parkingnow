83745eabe698bb4247d2d11472f54fe2af8c365b
chrislopsa <christian_lopez09@hotmail.com>
2024-09-13 13:14:37 -0500

    add untracked files

---
+++ src/bookings/services/create.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Booking } from "../entities/booking.entity";
+import { Repository } from "typeorm";
+import { CreateBookingDto } from "../dto/create-booking.dto";
+
+
+@Injectable()
+export class Create{
+    constructor(
+        @InjectRepository(Booking) private readonly bookingsRepository: Repository<Booking>
+    ){}
+
+    async create(bookingData: CreateBookingDto){
+        const newBooking = this.bookingsRepository.create(bookingData)
+        return await this.bookingsRepository.save(newBooking)
+    }
+}
