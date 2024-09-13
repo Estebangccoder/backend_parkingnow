ed598f5a64271d0dd0bb4514fb73cae3cd66bdc9
mspena14 <marlono1naranjo@gmail.com>
2024-09-12 21:06:13 -0500

    Fix: vehicle folder deleted

--- src/vehicles/vehicles.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Vehicle } from "./entities/vehicle.entity";
-
-@Module({
-    imports: [TypeOrmModule.forFeature([Vehicle])],
-})
-export class VehicleModule{}
