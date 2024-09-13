75e8d8e96cec0986b8fc5165ebcc3a5f136e5613
mspena14 <marlono1naranjo@gmail.com>
2024-09-13 15:03:49 -0500

    feat: cords added

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: '*'
+  });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
     forbidNonWhitelisted: true
-  }))
+  }));
   
   const config = new DocumentBuilder()
     .setTitle('Parking now API')
