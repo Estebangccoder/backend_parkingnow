96584a34c64f3340f2180158e1a5de7bc5cb58c6
mspena14 <marlono1naranjo@gmail.com>
2024-09-13 15:17:21 -0500

    feat: methods added

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*'
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({
