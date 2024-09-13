fc59d9d75d1dda0cb44bd81da80ba1eaedf72dac
mspena14 <marlono1naranjo@gmail.com>
2024-09-13 00:06:39 -0500

    feat: swagger configurations

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Parking now API')
+    .setDescription('This is the documentation for our API, here you will find all our endpoints, how to access them and what responses they will give. ')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
