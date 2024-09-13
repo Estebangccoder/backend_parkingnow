5da9c32d8a848a62657238efde96ed7aa27c94eb
estebangccoder <estebangccoder@gmail.com>
2024-09-13 12:14:26 -0500

    Fix:Delete vehicles

--- src/vehicles/dto/delete-vehicle.dto.ts
+++
@@ -1,7 +0,0 @@
-import {IsNotEmpty, IsString} from 'class-validator';
-
-export class DeleteVehicleDto{
-    @IsNotEmpty()
-    @IsString()
-    id: string
-}
--- src/vehicles/services/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './create.service';
-export * from './delete.service';
-export * from './find-vehicle-by-id.service';
-export * from './find-all.service';
