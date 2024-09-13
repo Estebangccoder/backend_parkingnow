2cf1b92fccc25facdec086ebae4e53a8d8f3f740
mspena14 <marlono1naranjo@gmail.com>
2024-09-13 00:28:12 -0500

    feat: dynamic filter added

--- src/slots/slots.controller.ts
+++ src/slots/slots.controller.ts
@@ -12,6 +12,7 @@ import {
 import { SlotsService } from "./slots.service";
 import { CreateSlotDto } from "./dto/create-slot.dto";
 import { UpdateSlotDto } from "./dto/update-slot.dto";
+import { FilterAvailablesDto } from "./dto/filter-availables-slot.dto";
 
 @Controller("slots")
 export class SlotsController {
@@ -30,6 +31,12 @@ export class SlotsController {
   @Get("findById/:id")
   findOne(@Param("id") id: string) {
     return this.slotsService.findOne(id);
+  }
+
+  @Get("available")
+  async getAvailableSlots(@Query() filterAvailables: FilterAvailablesDto) {
+
+    return this.slotsService.findAvailableSlotsByFilters(filterAvailables);
   }
 
   @Patch(":id")
