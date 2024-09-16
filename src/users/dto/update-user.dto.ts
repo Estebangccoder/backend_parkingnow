a9af9d597dc6997324d75bf235ebd7005c17d42d
mspena14 <marlono1naranjo@gmail.com>
2024-09-16 00:02:39 -0500

    feat: Users swagger documentation added and create.user.dto deleted

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,15 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateUserDto } from './create-user.dto';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsNumber, IsOptional } from 'class-validator';
+import { RegisterDto } from 'src/auth/dto/register.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+
+export class UpdateUserDto extends PartialType(RegisterDto) {
+
+    @ApiProperty({
+        description: 'User role ID',
+        example: 2
+    })
+    @IsOptional()
+    @IsNumber()
+    role_id?: number;
+}
