2811bd33833e9cc567c69c80de505bbfadcdbe51
chrislopsa <christian_lopez09@hotmail.com>
2024-09-24 19:50:14 -0500

    add handle error in /services/find-inprogress-driver.service.ts

--- src/auth/guard/auth.guard.ts
+++ src/auth/guard/auth.guard.ts
@@ -22,6 +22,7 @@ export class AuthGuard implements CanActivate {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: jwtConstants.secret,
       });
+      
       request['user'] = payload;
     } catch {
       throw new UnauthorizedException();
